b63dc0bbacc9290a50d4eb126985ff1f37f8f8ae
soroktree <soroktree@gmail.com>
2022-09-06 22:23:24 +0300

    add models and methods total for cart and orderable

---
+++ db/migrate/20220906181104_create_oderables.rb
@@ -0,0 +1,11 @@
+class CreateOderables < ActiveRecord::Migration[7.0]
+  def change
+    create_table :oderables do |t|
+      t.belongs_to :product, null: false, foreign_key: true
+      t.belongs_to :cart, null: false, foreign_key: true
+      t.integer :quantity
+
+      t.timestamps
+    end
+  end
+end
